60ffe0b4a3dcb16cd1e95a9e1a2138e624fcb92b
HRobertSI <hrobert25@hotmail.com>
2021-05-04 22:27:56 +0200

    Dodan

---
+++ registrationsuccessful.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <form action="store.php" method="POST">
+        <div style="text-align: center;">
+        <span>Enregistrement confirmé</span><br><br>
+        <input type="submit" name="entrerlemagasin" value="Entrer le magasin">
+        </div>
+        </form>
+             
+    </body>
+</html>
